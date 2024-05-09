49d8c2ba0e65de0cabb6926afc9c3453d29b848d
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-05-09 16:29:05 -0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+class Ticket {
+    private static int count = 0;
+    private int id;
+    private String issue;
+
+    public Ticket(String issue) {
+        this.id = ++count;
+        this.issue = issue;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+}
