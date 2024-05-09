ac206a705d8c37105524d011c44e8a28ed4439f2
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-05-09 16:44:16 -0300

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,20 +1,47 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 class Ticket {
-    private static int count = 0;
-    private int id;
-    private String issue;
+    private int ticketId;
+    private String title;
+    private String description;
+    private String status;
+    private String assignedTo;
 
-    public Ticket(String issue) {
-        this.id = ++count;
-        this.issue = issue;
+    public Ticket(int ticketId, String title, String description) {
+        this.ticketId = ticketId;
+        this.title = title;
+        this.description = description;
+        this.status = "Aberto";
+        this.assignedTo = "Não atribuído";
     }
 
-    public int getId() {
-        return id;
+    public int getTicketId() {
+        return ticketId;
     }
 
-    public String getIssue() {
-        return issue;
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
     }
 }
