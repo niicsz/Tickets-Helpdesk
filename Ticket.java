89f1a69577bc9144c1dcc1c4142af9b54fa80380
Nicolas Bezerra Bini <78541669+niicsz@users.noreply.github.com>
2024-05-13 14:25:42 -0300

    Update Class Ticket

--- Ticket.java
+++ Ticket.java
@@ -2,17 +2,16 @@ public class Ticket {
     private static int count = 0;
     private int id;
     private String issue;
+    private User user; 
 
-    public Ticket(String issue) {
+    public Ticket(String issue, User user) { 
         this.id = ++count;
         this.issue = issue;
+        this.user = user; 
     }
 
     public int getId() {
         return id;
     }
 
-    public String getIssue() {
-        return issue;
-    }
-}
+    public String
